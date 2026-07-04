a2bfdd4c5031738d5bbefca543aecce8b880b879
Chris Caron <lead2gold@gmail.com>
2026-07-04 14:46:55 -0400

    bumped version to work with Apprise v1.12.0

--- apprise_api/core/settings/__init__.py
+++ apprise_api/core/settings/__init__.py
@@ -36,7 +36,7 @@ APP_AUTHOR = "Chris Caron"
 APP_COPYRIGHT = "Copyright (C) 2026 Chris Caron <lead2gold@gmail.com>"
 APP_LICENSE = "MIT"
 APP_URL = "https://github.com/caronc/apprise-api"
-APP_VERSION = "1.5.0"
+APP_VERSION = "1.5.1"
 
 # Mirror the container's TZ environment variable so Django does not
 # override the process timezone with its own default (America/Chicago).
